a8b251de0e9cfcc904e23059170c37c77ff9b16f
James Carmichael <james@siteclick.co.uk>
2018-10-09 11:48:41 +0100

    Update 20181008091346-returns-add-due-date-up.sql

--- migrations/sqls/20181008091346-returns-add-due-date-up.sql
+++ migrations/sqls/20181008091346-returns-add-due-date-up.sql
@@ -1,4 +1,3 @@
-alter table "returns"."returns" drop constraint unique_return;
 
 alter table returns.returns
   add column due_date date;
@@ -10,7 +9,3 @@ set due_date = (date_trunc('month', end_date) + interval '2 month' - interval '1
 -- all future rows should have a due date
 alter table returns.returns
   alter column due_date set not null;
-
-alter table "returns"."returns"
-add constraint unique_return unique (regime, licence_type, licence_ref, start_date, end_date, due_date, return_requirement);
-
