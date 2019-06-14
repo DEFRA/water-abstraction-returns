b871f1c825167ca5214419a082b2b37e1e1e5f01
Mark Swaffer <mdswaffer@gmail.com>
2019-06-14 15:56:41 +0100

    Fix: Creates void returns endpoint
    As part of an error with the water service, this change adds a new means
    of marking returns as void for a given licence

--- src/modules/returns/routes.js
+++ src/modules/returns/routes.js
@@ -1,9 +1,29 @@
 const returnsApi = require('./returns');
 const versionsApi = require('./versions');
 const linesApi = require('./lines');
+const voidReturnsController = require('./void-returns');
+const Joi = require('joi');
+
+const voidReturns = {
+  method: 'PATCH',
+  handler: voidReturnsController.patchVoidReturns,
+  path: '/returns/1.0/void-returns',
+  config: {
+    description: 'For the given licence and valid return ids, marks other returns as void',
+    validate: {
+      payload: {
+        licenceNumber: Joi.string().required(),
+        licenceType: Joi.string().required(),
+        regime: Joi.string().required(),
+        validReturnIds: Joi.array().items(Joi.string())
+      }
+    }
+  }
+};
 
 module.exports = [
   ...returnsApi.getRoutes(),
   ...versionsApi.getRoutes(),
-  ...linesApi.getRoutes()
+  ...linesApi.getRoutes(),
+  voidReturns
 ];
