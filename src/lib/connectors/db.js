9c427e927ff86d62c3393df1ad2e80476dc37d4d
James Carmichael <james@siteclick.co.uk>
2019-12-19 15:23:54 +0000

    Use db.createPool from water-abstraction-helpers (#157)

--- src/lib/connectors/db.js
+++ src/lib/connectors/db.js
@@ -1,8 +1,10 @@
-const moment = require('moment');
-const pg = require('pg');
-const config = require('../../../config.js');
 
-const { Pool } = pg;
+require('dotenv').config();
+const pg = require('pg');
+const moment = require('moment');
+const helpers = require('@envage/water-abstraction-helpers');
+
+const config = require('../../../config.js');
 const { logger } = require('../../logger');
 
 // Set dates to format YYYY-MM-DD rather than full date/time string with timezone
@@ -10,28 +12,4 @@ pg.types.setTypeParser(1082, 'text', function (val) {
   return moment(val).format('YYYY-MM-DD');
 });
 
-const pool = new Pool(config.pg);
-
-pool.on('acquire', () => {
-  const { totalCount, idleCount, waitingCount } = pool;
-  if (totalCount === config.pg.max && idleCount === 0 && waitingCount > 0) {
-    logger.info(`Pool low on connections::Total:${totalCount},Idle:${idleCount},Waiting:${waitingCount}`);
-  }
-});
-
-function query (queryString, params) {
-  return new Promise((resolve, reject) => {
-    pool.query(queryString, params)
-      .then(res => {
-        resolve({
-          data: res.rows,
-          error: null
-        });
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-};
-
-module.exports = { query, pool };
+exports.pool = helpers.db.createPool(config.pg, logger);
