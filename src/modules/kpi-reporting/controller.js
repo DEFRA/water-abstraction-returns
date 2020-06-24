7bfd298e676aa3c5b3bfdc95e76cb0d985ca3740
stephan.hough <stephan.hough@rpa.gov.uk>
2020-06-24 15:34:22 +0100

    feat/water-2644 -- return data now camelCased

--- src/modules/kpi-reporting/controller.js
+++ src/modules/kpi-reporting/controller.js
@@ -2,17 +2,30 @@
 const repo = require('../../lib/repo/kpi-reporting');
 const Boom = require('@hapi/boom');
 const moment = require('moment');
+const { camelCase } = require('lodash');
+const deepMapKeys = require('deep-map-keys');
+
+/**
+ * Camel cases the keys of an object, or an array of objects.
+ * @param {Array|Object} data The array of objects, or object that is to
+ * have it's keys camel cased
+ */
+const camelCaseKeys = data => {
+  return deepMapKeys(data, camelCase);
+};
 
 const getReturnsKpiDataBySeason = async (request) => {
   const startDate = moment(request.query.startDate).format('YYYY-MM-DD');
   const endDate = moment(request.query.endDate).format('YYYY-MM-DD');
   const { isSummer } = request.query;
 
-  const dataRows = await repo.findReturnsKpiDataBySeason(startDate, endDate, isSummer);
-  if (dataRows.data.length < 1) {
+  const { data } = await repo.findReturnsKpiDataBySeason(startDate, endDate, isSummer);
+  if (data.length < 1) {
     return Boom.notFound(`Returns KPI data by season not found for start date ${startDate}, ` +
     `end date ${endDate} and is summer flag ${isSummer}`);
   }
-  return dataRows;
+  const returnData = camelCaseKeys(data);
+
+  return { data: returnData };
 };
 exports.getReturnsKpiDataBySeason = getReturnsKpiDataBySeason;
