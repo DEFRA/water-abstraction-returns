b1a7212c9ce67bc6cd69c1148188ae947a15b796
Tim <tim@tamimi.dev>
2020-11-11 18:32:19 +0000

    Mucking around.

--- src/modules/core/controller.js
+++ src/modules/core/controller.js
@@ -3,12 +3,8 @@ const statusConnector = require('../../lib/connectors/status');
 
 const getStatus = async () => {
   try {
-    if (statusConnector) {
-      const status = await statusConnector.getStatus();
-      return status;
-    } else {
-      return null;
-    }
+    const status = await statusConnector.getStatus();
+    return status;
   } catch (e) {
     throw Boom.badImplementation(e);
   }
