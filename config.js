16a72d44e803965d80593ea95fd23cf00bdf3d2e
Tim <tim@tamimi.dev>
2021-03-31 17:07:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = {
 
   pg: {
     connectionString: process.env.DATABASE_URL,
-    max: process.env.NODE_ENV === 'local' ? 20 : 7,
+    max: 20,
     idleTimeoutMillis: 10000,
     connectionTimeoutMillis: 5000
   },
