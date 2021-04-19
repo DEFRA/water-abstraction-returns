eff812bf0dd50c640a0b8c86d7208baaf0693ffb
James Carmichael <james@siteclick.co.uk>
2021-04-19 10:28:21 +0100

    Remove original KPI reporting implementation

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,11 @@
 const coreRoutes = require('./modules/core/routes');
 const returnsRoutes = require('./modules/returns/routes');
 const acceptanceTestRoutes = require('./modules/acceptance-tests/routes');
-const kpiReportingRoutes = require('./modules/kpi-reporting/routes');
 const returnCycleRoutes = require('./modules/return-cycles/routes');
 
 module.exports = [
   ...coreRoutes,
   ...returnsRoutes,
   ...acceptanceTestRoutes,
-  ...Object.values(kpiReportingRoutes),
   ...Object.values(returnCycleRoutes)
 ];
