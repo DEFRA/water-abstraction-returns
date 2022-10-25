52ae5754f2e1d9eca56ef1e9a865594297f6aac9
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2022-10-25 15:07:35 +0100

    Create `/health/info` endpoint (#315)
    https://github.com/DEFRA/water-abstraction-system/pull/8
    
    We create a new endpoint `/health/info` which will be used by our System repo to display info. Currently this will be this repo's version and current git commit hash.

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const coreRoutes = require('./modules/core/routes')
 const returnsRoutes = require('./modules/returns/routes')
 const acceptanceTestRoutes = require('./modules/acceptance-tests/routes')
 const returnCycleRoutes = require('./modules/return-cycles/routes')
+const healthRoutes = require('./modules/health/routes')
 
 module.exports = [
   ...coreRoutes,
   ...returnsRoutes,
   ...acceptanceTestRoutes,
-  ...Object.values(returnCycleRoutes)
+  ...Object.values(returnCycleRoutes),
+  ...Object.values(healthRoutes)
 ]
