602b772d2c39d2b69ce6ed37671ac70d963c9690
Rebecca Ransome <beckyrose200@aol.com>
2022-11-28 16:10:12 +0000

    Formatting comment

--- src/lib/camel-case-keys.js
+++ src/lib/camel-case-keys.js
@@ -10,13 +10,13 @@ const deepMapKeys = require('map-keys-deep-lodash')
 const camelCaseKeys = data =>
   deepMapKeys(data, (value, key) => toCamelCase(key))
 
-/* This regex is converting any string into Camel Case
+/**
+ * This regex is converting any string into Camel Case.
  * [^a-zA-Z0-9] Is the first part and it is matching any character except those inside
  * the square brackets. This could be a dash (-), space ( ), or underscore (_)
  * The next part of the regex +(.) is matching the first character after the dash, space or underscore.
  * The last part is the /g. This replaces all the global matches and not just the first match, so the whole string can be converted
 */
-
 function toCamelCase (key) {
   const result = key.toLowerCase()
   return result.replace(/[^a-zA-Z0-9]+(.)/g, (match, char) => char.toUpperCase())
