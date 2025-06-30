c76f6b7b6f90d639f50712942dfce3c785334cfc
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-06-30 08:45:56 -0700

    Update migrations/sqls/20250630125251-returns-id-up.sql
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- migrations/sqls/20250630125251-returns-id-up.sql
+++ migrations/sqls/20250630125251-returns-id-up.sql
@@ -1,5 +1,5 @@
-ALTER TABLE returns ADD COLUMN id UUID DEFAULT gen_random_uuid();
+ALTER TABLE "returns"."returns" ADD COLUMN id UUID DEFAULT gen_random_uuid();
 
-UPDATE returns SET id = gen_random_uuid();
+UPDATE "returns"."returns" SET id = gen_random_uuid();
 
-ALTER TABLE returns ALTER COLUMN id SET NOT NULL;
+ALTER TABLE "returns"."returns" ALTER COLUMN id SET NOT NULL;
