5e9442bd4e827774aea1e10ba6ca47b043b72609
Jason Claxton <jason.claxton@defra.gov.uk>
2025-10-01 16:06:13 +0100

    Wrap the query in an anonymous code block to hopefully get it to run

--- migrations/sqls/20250930135214-link-logs-to-requirements-up.sql
+++ migrations/sqls/20250930135214-link-logs-to-requirements-up.sql
@@ -6,28 +6,38 @@
   This migration will add a new return_requirement_id UUID field to the table returns.returns. It will then attempt to
   populate the return_requirement_id for existing records using the table water.return_requirements. It may not be
   possible to populate them all, which is why the return_requirement_id field will be left as nullable.
+
+  Note - We have to wrap the query in an anonymous code block because we depend on a table in another schema but when
+  this is run in CI that schema does not exist.
 */
 
-BEGIN;
+DO $$
+BEGIN
+  IF EXISTS
+    (
+      SELECT
+        1
+      FROM
+        information_schema.tables
+      WHERE
+        table_schema = 'water'
+        AND table_name = 'return_requirements'
+    )
+  THEN
+    ALTER TABLE "returns"."returns" ADD COLUMN return_requirement_id UUID;
 
-ALTER TABLE "returns"."returns" ADD COLUMN return_requirement_id UUID;
-
-WITH return_requirement_ids AS (
-  SELECT r.id, rrv.return_requirement_id
-  FROM "returns"."returns" r
-  INNER JOIN water.licences l
-    ON r.licence_ref = l.licence_ref
-  INNER JOIN (
-    SELECT rr.return_requirement_id, rr.return_version_id, rr.legacy_id, rv.licence_id
-    FROM water.return_requirements rr
-    INNER JOIN water.return_versions rv
-      ON rr.return_version_id = rv.return_version_id
+    UPDATE "returns"."returns" r
+    SET return_requirement_id = rrv.return_requirement_id
+    FROM water.licences l
+    INNER JOIN (
+      SELECT rr.return_requirement_id, rr.legacy_id, rv.licence_id
+      FROM water.return_requirements rr
+      INNER JOIN water.return_versions rv
+        ON rr.return_version_id = rv.return_version_id
     ) rrv
-    ON l.licence_id = rrv.licence_id AND r.return_requirement::integer = rrv.legacy_id
-)
-UPDATE "returns"."returns" r
-SET return_requirement_id = rri.return_requirement_id
-FROM return_requirement_ids rri
-WHERE r.id = rri.id;
-
-COMMIT;
+      ON l.licence_id = rrv.licence_id
+    WHERE r.licence_ref = l.licence_ref
+      AND r.return_requirement::integer = rrv.legacy_id;
+  END IF;
+END
+$$;
