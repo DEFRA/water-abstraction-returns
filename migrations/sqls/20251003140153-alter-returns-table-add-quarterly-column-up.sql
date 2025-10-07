9ccec1f7a50cf906c53499fcefaef3b6921bc7ae
robertparkinson <robert@daedalusft.com>
2025-10-07 15:08:02 +0100

    add documentation

--- migrations/sqls/20251003140153-alter-returns-table-add-quarterly-column-up.sql
+++ migrations/sqls/20251003140153-alter-returns-table-add-quarterly-column-up.sql
@@ -1,3 +1,12 @@
+/*
+  Adds and populates a new quaterly boolean field to the table returns.returns
+
+  https://eaflood.atlassian.net/browse/WATER-5255
+
+  This migration will add a new quarterly boolean field to the table returns.returns. It will then set the value
+  to true for all return logs that match a quarertly return version.
+*/
+
 ALTER TABLE "returns"."returns" ADD COLUMN quarterly boolean default false not null;
 
 WITH quarterly_return_requirements AS (
