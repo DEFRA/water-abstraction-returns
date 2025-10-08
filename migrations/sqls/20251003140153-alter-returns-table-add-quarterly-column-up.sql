76309ed1633e979b3904a45c236c12eb6581ce05
Robert Parkinson <robertparkinson@users.noreply.github.com>
2025-10-08 12:36:05 +0100

    Add quarterly flag to returns table (#445)
    https://eaflood.atlassian.net/browse/WATER-5255
    
    As a lot of reports are driven by the returns table we're adding a column to flag the return as quarterly. This saves us having to join out to the return version table which should optimise the reports that use this table.

---
+++ migrations/sqls/20251003140153-alter-returns-table-add-quarterly-column-up.sql
@@ -0,0 +1,42 @@
+/*
+  Adds and populates a new quaterly boolean field to the table returns.returns
+
+  https://eaflood.atlassian.net/browse/WATER-5255
+
+  This migration will add a new quarterly boolean field to the table returns.returns. It will then set the value
+  to true for all return logs that match a quarertly return version.
+
+  Note - We have to wrap the query in an anonymous code block because we depend on a table in another schema but when
+  this is run in CI that schema does not exist.
+*/
+
+DO $$
+BEGIN
+  IF EXISTS
+    (
+      SELECT
+        1
+      FROM
+        information_schema.tables
+      WHERE
+        table_schema = 'water'
+        AND table_name = 'return_requirements'
+    )
+  THEN
+    ALTER TABLE "returns"."returns" ADD COLUMN quarterly boolean default false not null;
+
+    WITH quarterly_return_requirements AS (
+      SELECT rr.return_requirement_id
+      FROM water.return_requirements rr
+      INNER JOIN water.return_versions rv
+        ON rr.return_version_id = rv.return_version_id
+      WHERE rv.quarterly_returns = true
+    )
+
+    UPDATE "returns"."returns" r
+    SET quarterly = true
+    FROM quarterly_return_requirements qrr
+    WHERE r.return_requirement_id = qrr.return_requirement_id;
+  END IF;
+END
+$$;
