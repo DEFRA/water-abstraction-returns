3c1a2420910fbcf336ccf8469900cf406882e7be
Jason Claxton <jason.claxton@defra.gov.uk>
2025-12-02 16:25:48 +0000

    Update script comment

--- migrations/sqls/20251202161002-round-return-lines-quantities-up.sql
+++ migrations/sqls/20251202161002-round-return-lines-quantities-up.sql
@@ -2,6 +2,9 @@
   Rounds existing return submission line quantities to 6 decimal places
 
   https://eaflood.atlassian.net/browse/WATER-5398
+
+  The WHERE clause speeds up the update from around +10 minutes to around 25 seconds when tested on dev by only
+  targeting rows where the quantity is not NULL or already rounded to 6 decimal places.
 */
 
 UPDATE "returns".lines
