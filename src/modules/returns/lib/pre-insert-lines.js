7da464b555d9a879043585e508e98aa0fc572917
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-02-02 19:59:46 +0000

    Implement fix

--- src/modules/returns/lib/pre-insert-lines.js
+++ src/modules/returns/lib/pre-insert-lines.js
@@ -9,9 +9,13 @@
 const preInsert = async data => {
   const lines = Array.isArray(data) ? data : [data]
   return lines.map(line => {
+    const quantity = line.quantity
+
+    // NOTE: We can't do a standard `quantity ? true : false` because it is valid to submit a line quantity of 0 and we
+    // want to persist that to the DB. 0 in Node.js is interpreted as being falsy, which is not what we want here.
     return {
       ...line,
-      quantity: line.quantity ? Number(line.quantity.toFixed(6)) : null
+      quantity: quantity !== undefined && quantity !== null ? Number(quantity.toFixed(6)) : null
     }
   })
 }
